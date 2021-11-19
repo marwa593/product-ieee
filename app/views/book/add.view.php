bad39e00cd0b9e0ff22d7da243585f8cee864626
marw593 <marwa.mamm79@gmail.com>
2021-11-20 00:11:14 +0200

    back-end files

--- app/views/book/add.view.php
+++ app/views/book/add.view.php
@@ -1,33 +1,41 @@
 <?php
 
 ?>
-
-<html>
-
-<form  method="post" >
-    <fieldset>
-        <legend>LOG IN </legend>
-
-        <label>name</label>
-        <input type="text"  name="name"> <br><br><br>
-
-        <label>category</label>
-        <input type="text"  name="category"> <br><br><br>
-
-        <label>pages_num</label>
-        <input type="text"  name="pages_num"> <br><br><br>
-
-        <label>writer</label>
-        <input type="text"  name="writer"> <br><br><br>
-
-        <label>price</label>
-        <input type="int"  name="price"> <br><br><br>
-
-
-        <input type="submit" name="sub" value ="Add">
-
-
-    </fieldset>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="stylesheet" href="../../public/css/bootstrap.css"/>
+<link rel="stylesheet" href="../../public/css/iee-task-2-edite.css"/>
+  </head>
+  <body>
+<div class="container">
+  <div class="add-form">
+    <form action="ieee-task-2.html" target="" method="post">
+      <ul>
+        <li class="product-li">
+    <input type="text" placeholder="BOOK NAME" name="name" class="text-center product-input">
+  </li>
+  <li  class="product-li">
+    <input type="text" placeholder="BOOK AUTHOR" name="author" class="text-center product-input">
+  </li>
+<li  class="product-li">
+    <input type="number" placeholder="BOOK PAGES" name="pages" class="text-center product-input">
+  </li>
+  <li  class="product-li">
+  <input type="text" placeholder="BOOK CATEGORY" name="category" class="text-center product-input">
+</li>
+<li  class="product-li">
+  <input type="number" placeholder="BOOK PRICE" name="price" class="text-center product-input">
+</li>
+<li   class="product-li">
+  <input type="submit" class="btn btn-danger add-botton " value="Add">
+</li>
+</ul>
 </form>
-
+</div>
+</div>
+  </body>
 </html>
